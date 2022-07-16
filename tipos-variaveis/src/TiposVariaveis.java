c3a884cb285941e8d70f277a258bd2b05880ebbd
DanielMelo1 <dreimao4@gmail.com>
2022-07-16 00:27:24 -0300

    Projeto-Smart-tv

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,25 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+        //estudem a classe String que representa texto na aplicação
+
+        String meuNome = "Daniel Melo";
+        
+        double salarioMinimo = 2500;
+
+
+        short numeroCurto = 1;
+        int numeronormal = numeroCurto;
+        short numeroCurto2 = (short) numeronormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+
     }
 }
